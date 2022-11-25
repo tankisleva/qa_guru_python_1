e00728e3549073d28968e1609d85b47b774bddbe
Oleg Malyshev <malyshev@jivosite.com>
2022-11-25 22:21:20 +0300

    edit fixture name

--- conftest.py
+++ conftest.py
@@ -3,6 +3,6 @@ from selene.support.shared import browser
 
 
 @pytest.fixture(scope="session")
-def open_browser():
+def set_size_browser():
     browser.config.window_width = 300
     browser.config.window_height = 100
