0b97d5b99be498d38a965d5a1f01028903f9e440
Oleg Malyshev <malyshev@jivosite.com>
2022-11-27 16:46:26 +0300

    edit tests

--- conftest.py
+++ conftest.py
@@ -6,3 +6,8 @@ from selene.support.shared import browser
 def set_size_browser():
     browser.config.window_width = 300
     browser.config.window_height = 100
+
+
+@pytest.fixture()
+def open_browser():
+    browser.open('https://google.ru')
